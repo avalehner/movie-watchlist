ac66ca9cdd690457c30f211b349d2f83438f07bd
Ava Lehner <avalehner@gmail.com>
2025-05-12 17:32:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const searchInput = document.getElementById('search-input')
+const searchBtn = document.getElementById('search-btn')
+const movieListContainer = document.getElementById('movie-list-container')
+const moviesFromLocalStorage = JSON.parse(localStorage.getItem('movies'))
+let searchListArr = []
+let imdbIDArr = []
+let watchlistArr = []
+
+//get movies from local storage 
+if (moviesFromLocalStorage) {
+  watchlistArr = moviesFromLocalStorage
+}
+
+// event listeners 
+searchBtn.addEventListener('click', ()=>{
+  document.getElementById('main-container').style.alignItems = 'flex-start'
+  document.getElementById('main-container').style.height = '100%'
+  document.getElementById('explore-img').style.display = 'none'
+  document.getElementById('error-msg').style.display = 'none'
+  movieListContainer.style.display = 'block'
+
+  fetchMoviesFromSearch()
+})
+
+document.addEventListener ('click', (e) => {
+  if (e.target.dataset.add) {
+    addWatchlist(e.target.dataset.add)
+  }
+})
+
+searchInput.addEventListener('keypress', (e) => {
+  if (e.key == 'Enter') searchBtn.click()
+})
+
+// functions
+
+function fetchMoviesFromSearch () {
+  const movieTitle = searchInput.value
+  if (!movieTitle) return
+
+  fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=2d86dd0a`)
+    .then(res => res.json())
+    .then(movies => {
+      if (movies.Response === "False") {
+        document.getElementById('error-msg').style.display = 'block'
+      } else {
+        const moviesArr = movies.Search
+
+        //get imdbID from returned array and add to searchArr
+        moviesArr.forEach(movie => imdbIDArr.push(movie.imdbID))
+
+        //render movies for each imdbID
+        imdbIDArr.forEach(imdbID => renderMoviesFromId(imdbID))
+      }
+  })
+}
+
+function renderMoviesFromId(imdbID) {
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=2d86dd0a`)
+    .then(res => res.json())
+    .then(movies => {
+        let searchListHtml = `
+          <div class="movie-container" id="${movies.imdbID}">
+            <img class="poster" src="${movies.Poster}">
+            <div class="movie-info-container">
+              <div class="movie-title-container">
+                <h3 class="movie-title">${movies.Title}</h3>
+                <img class="star-icon" src="images/star-icon.png">
+                <p class="movie-rating">${movies.imdbRating}
+              </div>
+              <div class="movie-data-container">
+                <p class="runtime">${movies.Runtime}</p>
+                <p class="genre">${movies.Genre}</p>
+                <div id="add-watchlist-container" class="add-watchlist-container">
+                  <img id="add-icon" class="add-icon" 
+                  src="images/add-icon.png" 
+                  data-add="${movies.imdbID}">
+                  <p class="watchlist">Watchlist</p>
+                </div>
+              </div>
+              <p class="movie-plot">${movies.Plot}</p>
+            </div>
+          </div>
+        <hr/>`
+        movieListContainer.innerHTML += searchListHtml
+    })
+}
+
+function addWatchlist(imdbID) {
+  if (watchlistArr.includes(imdbID)) return 
+  watchlistArr.push(imdbID)
+
+  // add movies to local storage for watchlist 
+  localStorage.setItem('movies', JSON.stringify(watchlistArr))
+
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=2d86dd0a`)
+  .then(res => res.json())
+  .then(movie => {alert(`Added '${movie.Title}' to watchlist!`)})
+}
---
+++ watchlist.js
@@ -0,0 +1,80 @@
+let watchlistArr = []
+const watchlistContainer = document.getElementById('watch-list-container')
+const moviesFromLocalStorage = JSON.parse(localStorage.getItem('movies'))
+
+handleWatchlistDisplay()
+
+// event listeners 
+document.addEventListener ('click', (e) => {
+  if (e.target.dataset.remove) {
+    removeWatchlist(e.target.dataset.remove)
+  }
+})
+
+// functions 
+function renderMoviesFromId(imdbID) {
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=2d86dd0a`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+
+        let watchlistHtml = `
+        <div id="${data.imdbID}">
+          <div class="movie-container">
+            <img class="poster" src="${data.Poster}">
+            <div class="movie-info-container">
+              <div class="movie-title-container">
+                <h3 class="movie-title">${data.Title}</h3>
+                <img class="star-icon" src="images/star-icon.png"/>
+                <p class="movie-rating">${data.imdbRating}
+              </div>
+              <div class="movie-data-container">
+                <p class="runtime">${data.Runtime}</p>
+                <p class="genre">${data.Genre}</p>
+                <div id="remove-watchlist-container" class="remove-watchlist-container">
+                  <img id="remove-icon" class="remove-icon" 
+                  src="images/remove-icon.png" 
+                  data-remove="${data.imdbID}">
+                  <p class="remove-icon">Remove</p>
+                </div>
+              </div>
+              <p class="movie-plot">${data.Plot}</p>
+            </div>
+          </div>
+          <hr/>
+        </div>`
+      
+      watchlistContainer.innerHTML += watchlistHtml
+  })
+}
+
+
+function removeWatchlist(imdbID) {
+  const removedMovieIndex = watchlistArr.indexOf(imdbID)
+  watchlistArr.splice(removedMovieIndex, 1)
+  console.log(watchlistArr)
+
+  // setting local storage again so removed item is gone
+  localStorage.setItem('movies', JSON.stringify(watchlistArr))
+
+  //removing item visually 
+  document.getElementById(imdbID).style.display = 'none'
+
+  //reload empty message
+  handleWatchlistDisplay ()
+
+}
+
+function handleWatchlistDisplay () {
+  if (moviesFromLocalStorage.length !== 0) {
+    //get movies from local storage 
+      watchlistArr = moviesFromLocalStorage
+      watchlistArr.forEach(imdbID => renderMoviesFromId(imdbID))
+    
+      document.getElementById('empty-watchlist-msg').style.display ='none'
+      watchlistContainer.style.display = 'block'
+    } else {
+       document.getElementById('empty-watchlist-msg').style.display ='block'
+       watchlistContainer.style.display = 'none'
+    }
+}
